c27f0e8ab12a54997e08ab2794a665da4e8323ef
Alexis Devillard <long.devillard@gmail.com>
2019-05-20 13:29:09 +0200

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,24 @@ int main(int argc, char ** argv)
   std::vector<std::string> opt_flag(
 				    {"-ni",
 					"-nl",
-					"-nb"});
+					"-nbi",
+					"-nbl"});
   std::vector<std::string> opt_label(
 				     {"Lsl sample stream's name",
 					 "Lsl label stream's name",
-					 "Number of channels"});
+					 "Number of channels of the input",
+					 "Number of channels of the label"});
   std::vector<std::string> opt_value(
 				     {"generator_sample",
 					 "generator_label",
-					 "5"});
+					 "4",
+					 "15"});
   get_arg(argc, argv, opt_flag, opt_label, opt_value);
   
   std::string stream_sample_name = opt_value[0];
   std::string stream_label_name = opt_value[1];
   int nb_ch = std::stoi(opt_value[2]);
-
+  int nb_ch_l = std::stoi(opt_value[3]);
 
   int t = 0;
   srand (time(NULL));
@@ -36,21 +39,20 @@ int main(int argc, char ** argv)
   try
     {
       lsl::stream_info info_sample(stream_sample_name, "sample", nb_ch, 0, lsl::cf_float32);
-      lsl::stream_info info_label(stream_label_name, "label", 1, 0, lsl::cf_float32);
+      lsl::stream_info info_label(stream_label_name, "label", nb_ch_l, 0, lsl::cf_float32);
       lsl::stream_outlet outlet_sample(info_sample);
       lsl::stream_outlet outlet_label(info_label);
 
       std::vector<float> sample(nb_ch);
-      std::vector<float> label(1);
+      std::vector<float> label(nb_ch_l);
       std::cout << "[INFOS] Now sending data... " << std::endl;  
       for(;;t++)
 	{
 	  //generate
 	  for(int i = 0; i< sample.size(); i++)
-	    sample[i] = ((t/100)%2)?1:0;
+	    sample[i] = (((t+i)/100)%2)?-0.5:0.5;
 	  
-	  //label
-	  label[0] = (sample[0]==1)?1:0;
+	  
 
 	  //noise
 	  int a =50;
@@ -58,7 +60,16 @@ int main(int argc, char ** argv)
 	  for(int i = 0; i<sample.size(); i++)
 	    {
 	      sample[i] += (float)( rand() % (2*a) - a) / (float)a   *amp;
-	      sample[i] = (sample[i]<0)?0:(sample[i]>1)?1:sample[i];
+	      // sample[i] = (sample[i]<0)?0:(sample[i]>1)?1:sample[i];
+	    }
+
+	  //label
+	  for(int i = 0; i< label.size(); i++)
+	    label[i] = 10*sample[i%sample.size()] * sample[(i+1)%sample.size()];
+
+	  for(int i = 0; i<label.size(); i++)
+	    {
+	      //label[i] = (label[i]<0)?0:(label[i]>1)?1:label[i];
 	    }
 	    
 	  // send it
